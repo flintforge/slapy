a11d3a5e89d142051d7450c308d9518953b4fbf3
Philippe Estival <flint@forge.systems>
2016-05-28 23:54:13 +0200

    config&fixtures

---
+++ config.py
@@ -0,0 +1,11 @@
+
+
+HOSTS="./hosts"
+MUNINCONFD="/etc/munin/munin-conf.d"
+ansible="/opt/ansible"
+ANSIBLE_REALM="/opt/ansible/realm"
+HaProxyHost="admin@loadbal"
+HaProxyPath="/etc/haproxy/"
+debug=True
+
+
